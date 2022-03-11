06bd03eec8d92e334fca06d20ee8e7a6dc344a8b
afdalabdallah <afdalabdallahm@gmail.com>
2022-03-11 23:14:54 +0700

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QUIS_1.Models;
+using System.Diagnostics;
+
+namespace QUIS_1.Controllers
+{
+    public class Q1Controller : Controller
+    {
+        private readonly ILogger<Q1Controller> _logger;
+
+        public Q1Controller(ILogger<Q1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult profile()
+        {
+            return View();
+        }
+        public IActionResult service()
+        {
+            return View();
+        }
+        public IActionResult collaboration()
+        {
+            return View();
+        }
+        public IActionResult company()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Views/q1/company.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QUIS_1.Views.q1
+{
+    public class companyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
